0be5ba3a8e3bc44bc2a334dff9458cca0a2b9973
Sorel Mitra <sorelmitra@yahoo.com>
2022-10-13 18:23:42 +0300

    feat: allow overriding .env values with environment variables

--- src/lib/config_tool.py
+++ src/lib/config_tool.py
@@ -6,5 +6,6 @@ from dotenv import dotenv_values
 class Config:
     _ENV = os.environ['ENV']
     value = {
-        **dotenv_values(f".{_ENV}.env")
+        **dotenv_values(f".{_ENV}.env"),
+        **os.environ
     }
