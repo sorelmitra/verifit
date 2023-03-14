97a83d423539d0811b379bf0063a41121f073a7c
Sorel Mitra <sorelmitra@yahoo.com>
2023-03-14 19:28:40 +0200

    Simplify date diff

--- src/verifit/date_and_time.py
+++ src/verifit/date_and_time.py
@@ -1,24 +1,8 @@
-def date_diff(a_date):
+def date_diff_in_minutes(a_date):
     def with_date(date_to_subtract):
         difference = a_date - date_to_subtract
-
-        def apply(func=None):
-            if func is None:
-                return difference
-            return func(difference)
-        return apply
-    return with_date
-
-
-def in_minutes():
-    def apply_to(difference):
         minutes = difference.seconds // 60
         if difference.days < 0:
             minutes = minutes + difference.days * 24 * 60
-
-        def with_func(func=None):
-            if func is None:
-                return minutes
-            return func(minutes)
-        return with_func
-    return apply_to
+        return minutes
+    return with_date
--- tests/shopping-service/login/test_login.py
+++ tests/shopping-service/login/test_login.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from verifit.date_and_time import date_diff, in_minutes
+from verifit.date_and_time import date_diff_in_minutes
 from verifit.driver import get_driver
 from verifit.login import get_expiry_date, login, login_main_user
 
@@ -8,11 +8,11 @@ from verifit.login import get_expiry_date, login, login_main_user
 def test_login_main_user(shopping_driver_name):
     user = get_driver(shopping_driver_name)('user')('MAIN')
     login_data = login(user)(get_driver(shopping_driver_name)('login'))
-    minutes = date_diff(get_expiry_date(login_data))(datetime.now())(in_minutes())()
+    minutes = date_diff_in_minutes(get_expiry_date(login_data))(datetime.now())
     assert 58 < minutes < 61
 
 
 def test_login_main_user_with_shortcut(shopping_driver_name):
     login_data = login_main_user(shopping_driver_name)
-    minutes = date_diff(get_expiry_date(login_data))(datetime.now())(in_minutes())()
+    minutes = date_diff_in_minutes(get_expiry_date(login_data))(datetime.now())
     assert 58 < minutes < 61
