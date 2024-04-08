3577f9b684e995497a80ed66a324b89436325f60
Sorel Mitra <sorelmitra@yahoo.com>
2024-04-08 14:47:28 +0300

    chore: remove debug print

--- src/verifit/http_server.py
+++ src/verifit/http_server.py
@@ -153,7 +153,6 @@ def graphql_server():
         debug=api().debug
     )
     status_code = 200 if success else 400
-    print('XXXXXXXX', result, status_code)
     return jsonify(result), status_code
 
 
